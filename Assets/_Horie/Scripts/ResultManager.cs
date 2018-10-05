5b34228c9ea58b28043233aa34e459018c87e78e
HorieYuki <horie.yuki.as@gmail.com>
2018-10-05 19:26:35 +0900

    Resultスクリプト作成

---
+++ Assets/_Horie/Scripts/ResultManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour {
+    // 起動関数を呼び出すとスコアのカウントとか始まる
+
+    // scoreCounter
+    [SerializeField] private GameObject _ResultLogo;
+    [SerializeField] private GameObject _ScoreCounter;
+    [SerializeField] private GameObject _RankingManager;    
+    [SerializeField] private float _scoreCounterStartTime;      // ResultLogo出てからスコア回転開始までの時間
+    [SerializeField] private float _rankingManagerStartTime;    // score回転終了してからランキング起動までの時間
+    // 稼働中フラグ
+    private bool isEnableResultManager;
+    private bool isFinishScoreCounter;
+
+    // 獲得スコア
+    private int getScore;
+
+    // 時間カウンタ
+    private float fTime;
+
+    private bool bRunningRankingManager;
+
+	// Use this for initialization
+	void Start () {
+
+        isEnableResultManager = false;
+        isFinishScoreCounter = false;
+        // くっついてるゲームオブジェクトすべて非アクティブ化
+        // 起動のときに起こす
+        _ResultLogo.SetActive(false);
+        _ScoreCounter.SetActive(false);
+        _RankingManager.GetComponent<RankingManager>().InitRankingManager();
+        _RankingManager.SetActive(false);
+        bRunningRankingManager = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 起動中でない場合は返す
+        if (!isEnableResultManager)
+            return;
+
+        // スコアカウンタがアクティブでない場合
+        if (bRunningRankingManager == false && !_ScoreCounter.activeSelf && _scoreCounterStartTime < fTime)
+        {
+            // スコアカウンタの起動
+            _ScoreCounter.SetActive(true);
+            _ScoreCounter.GetComponent<ScoreCounter>().SetScore(getScore);
+        }
+
+        // スコア回転終了してマクロ時間経過した場合
+        if (bRunningRankingManager == false && isFinishScoreCounter == true && _rankingManagerStartTime < fTime)
+        {
+            // ResultLogoとScoreの表示終了
+            _ResultLogo.SetActive(false);
+            _ScoreCounter.SetActive(false);
+
+            // RankingManagerの起動
+            _RankingManager.SetActive(true);
+            _RankingManager.GetComponent<RankingManager>().StartRankingManager(getScore);
+
+            bRunningRankingManager = true;
+        }
+
+        // タイムカウンタ回転
+        fTime += Time.deltaTime;
+    }
+
+    // Resultモード起動関数
+    public void StartResult( int score )
+    {
+        isEnableResultManager = true;
+
+        // スコア受け取り
+        getScore = score;
+
+        // リザルトロゴの起動
+        _ResultLogo.SetActive(true);
+    }
+
+    // Scoreカウンタから終了フラグをもらう
+    public void GetFinishScoreCounter ( )
+    {
+        isFinishScoreCounter = true;
+
+        // タイムカウンタ初期化
+        fTime = 0;
+    }
+}
