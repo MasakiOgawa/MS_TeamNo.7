a01153da40e06dc356d989265e4d341c95fdb164
HORIE_YUKI_DESKPC <vipstar.final@gmail.com>
2018-10-25 23:51:30 +0900

    timeline編集アセット追加

---
+++ Assets/_Horie/Scripts/CutScene/CutScene0.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class CutScene0 : MonoBehaviour {
+
+
+    private PostProcessingBehaviour behaviour;
+    
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    // timeline最初に呼び出す
+    public void Init()
+    {
+        // postProcessingBehaviourを取得
+        behaviour = GetComponent<PostProcessingBehaviour>();
+
+        // postprocess情報を初期化
+        Initialize();
+
+    }
+
+    private void Initialize()
+    {
+        // 初期化
+        var Settings = behaviour.profile.bloom.settings;
+        Settings.bloom.intensity = 0.65f;
+        behaviour.profile.bloom.settings = Settings;
+    }
+}
