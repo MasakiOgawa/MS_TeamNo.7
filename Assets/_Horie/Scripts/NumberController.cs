5a5b3acf348ba74aad0de1792d6dd761303d13eb
HorieYuki <horie.yuki.as@gmail.com>
2018-11-05 14:05:12 +0900

    Modelsフォルダ作成

---
+++ Assets/_Horie/Scripts/NumberController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberController : MonoBehaviour {
+
+    // 表示したい数字を受け取ってそのテクスチャを表示するだけ
+
+    [SerializeField] private Sprite[] spriteObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
