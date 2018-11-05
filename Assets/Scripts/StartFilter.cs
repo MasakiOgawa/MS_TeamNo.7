12d979df61bae78329af9f5b2e21bf5564f833ed
DESKTOP-1RMC40N\syunta_makino <makino.syunta@outlook.jp>
2018-11-05 13:20:23 +0900

    牧野_ゲーム開始フェードの追加

---
+++ Assets/Scripts/StartFilter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartFilter : MonoBehaviour
+{
+    Image FilterImage;
+    float fAlpha;
+    public float fAddAlpha;
+
+
+	void Start( )
+    {
+        fAlpha = 1.0f;
+        FilterImage = this.GetComponent< Image >( );
+	}
+
+
+    public void AddAlpha( )
+    {
+        fAlpha -= fAddAlpha;
+        FilterImage.color = new Color( 255.0f , 255.0f , 255.0f , fAlpha );
+
+        if( fAlpha <= 0.0f )
+        {
+            fAlpha = 0.0f;
+            FilterImage.color = new Color( 255.0f , 255.0f , 255.0f , 0.0f );
+        }
+    }
+}
