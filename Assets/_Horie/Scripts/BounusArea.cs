f56edd95452b1d68dcae7544775d7959dd2bf64a
HorieYuki <horie.yuki.as@gmail.com>
2018-11-13 01:38:05 +0900

    BounusArea作成。使い方はtrello

--- Assets/_Horie/Scripts/BounusArea.cs
+++ Assets/_Horie/Scripts/BounusArea.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class BounusArea : MonoBehaviour
 {
+    // 自身のゲームオブジェクト
+    static GameObject BounusAreaObj;
 
-    [SerializeField] private GameObject Area1;
-    [SerializeField] private GameObject Area2;
-    [SerializeField] private GameObject Area3;
+
+    [SerializeField] private GameObject Area_Green;
+    [SerializeField] private GameObject Area_Green1;
+    [SerializeField] private GameObject Area_Green2;
+
 
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -21,11 +24,86 @@ public class BounusArea : MonoBehaviour
 
     }
 
-    // Init
-    static void Init()
+    // 
+    static public void Create( Vector3 pos0 , Vector3 pos1 , Vector3 pos2)
     {
-        
+        GameObject prefab;
+        prefab = (GameObject)Resources.Load("Prefabs/Bounus/BounusAreaManager");
+
+        GameObject obj = Instantiate(prefab);
+
+        // 生成したオブジェクトを保持
+        BounusAreaObj = obj;
+
+        BounusAreaObj.GetComponent<BounusArea>().Run(pos0, pos1, pos2);
+    }
+
+    // 終了
+    static public void Delete()
+    {
+
+        Destroy(BounusAreaObj);
+    }
+
+    // 色変更
+    static public void ChangeColor ( int nIdx , bool isGreen )
+    {
+        BounusArea Ba = BounusAreaObj.GetComponent<BounusArea>();
+
+        if (nIdx == 0)
+        {
+            if ( isGreen == true )
+            {
+                //Ba.Area_Green.SetActive(true);
+                Ba.Area_Green.GetComponent<BounusAreaController>().ChangeColor(true);
+                //Ba.Area_Yellow.SetActive(false);
+            }
+            else
+            {
+                Ba.Area_Green.GetComponent<BounusAreaController>().ChangeColor(false);
+                //Ba.Area_Green.SetActive(false);
+                //Ba.Area_Yellow.SetActive(true);
+            }
+        }
+        else if (nIdx == 1)
+        {
+            if (isGreen == true)
+            {
+                Ba.Area_Green1.GetComponent<BounusAreaController>().ChangeColor(true);
+            }
+            else
+            {
+                Ba.Area_Green1.GetComponent<BounusAreaController>().ChangeColor(false);
+            }
+        }
+        else if ( nIdx == 2)
+        {
+            if (isGreen == true)
+            {
+                Ba.Area_Green2.GetComponent<BounusAreaController>().ChangeColor(true);
+            }
+            else
+            {
+                Ba.Area_Green2.GetComponent<BounusAreaController>().ChangeColor(false);
+            }
+        }
+    }
+
+
+    private void Run(Vector3 pos0, Vector3 pos1, Vector3 pos2)
+    {
+        // 緑のみ点灯
+        Area_Green.SetActive(true);
+        Area_Green1.SetActive(true);
+        Area_Green2.SetActive(true);
+
+        // 座標設定
+        Area_Green.transform.position = pos0;
+        Area_Green1.transform.position = pos1;
+        Area_Green2.transform.position = pos2;
 
     }
+
+
 
 }
---
+++ Assets/_Horie/Scripts/BounusAreaController.cs
@@ -0,0 +1,318 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounusAreaController : MonoBehaviour {
+
+    [SerializeField] private GameObject Vertical;
+    [SerializeField] private GameObject GlowBits;
+    [SerializeField] private GameObject BigGlow;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeColor ( bool bGreen )
+    {
+        ChangeColorThis(bGreen);
+        ChangeColorVertical(bGreen);
+        ChangeColorGlowBits(bGreen);
+        ChangeColorBigGlow(bGreen);
+
+    }
+
+    // 自身の色変更
+    private void ChangeColorThis(bool bGreen)
+    {
+        Color col;
+        if (bGreen == true)
+            col = Color.green;
+        else
+            col = Color.yellow;
+        // ――――――――――――――――――――――――――――――――――――
+        //
+        // ――――――――――――――――――――――――――――――――――――
+        //Create Gradient key Min
+        GradientColorKey[] gradientColorKeyMin;
+        gradientColorKeyMin = new GradientColorKey[2];
+        gradientColorKeyMin[0].color = col;
+        gradientColorKeyMin[0].time = 0f;
+        gradientColorKeyMin[1].color = col;
+        gradientColorKeyMin[1].time = 1f;
+
+        //Create Gradient alpha Min
+        GradientAlphaKey[] gradientAlphaKeyMin;
+        gradientAlphaKeyMin = new GradientAlphaKey[4];
+        gradientAlphaKeyMin[0].alpha = 0.0f;
+        gradientAlphaKeyMin[0].time = 0.0f;
+        gradientAlphaKeyMin[1].alpha = (float)255.0f / (float)255.0f;
+        gradientAlphaKeyMin[1].time = 0.305f;
+        gradientAlphaKeyMin[2].alpha = 173.0f / 255.0f;
+        gradientAlphaKeyMin[2].time = 0.702f;
+        gradientAlphaKeyMin[3].alpha = 0f;
+        gradientAlphaKeyMin[3].time = 1f;
+
+        //Create Gradient key Max
+        GradientColorKey[] gradientColorKeyMax;
+        gradientColorKeyMax = new GradientColorKey[2];
+        gradientColorKeyMax[0].color = col;
+        gradientColorKeyMax[0].time = 0f;
+        gradientColorKeyMax[1].color = col;
+        gradientColorKeyMax[1].time = 1f;
+
+        //Create Gradient alpha Max
+        GradientAlphaKey[] gradientAlphaKeyMax;
+        gradientAlphaKeyMax = new GradientAlphaKey[4];
+        gradientAlphaKeyMax[0].alpha = 0.0f;
+        gradientAlphaKeyMax[0].time = 0.0f;
+        gradientAlphaKeyMax[1].alpha = (float)255.0f / (float)255.0f;
+        gradientAlphaKeyMax[1].time = 0.305f;
+        gradientAlphaKeyMax[2].alpha = 173.0f / 255.0f;
+        gradientAlphaKeyMax[2].time = 0.702f;
+        gradientAlphaKeyMax[3].alpha = 0f;
+        gradientAlphaKeyMax[3].time = 1f;
+
+        //Create Gradient Min
+        Gradient gradientMin = new Gradient();
+        gradientMin.SetKeys(gradientColorKeyMin, gradientAlphaKeyMin);
+
+        //Create Gradient Max
+        Gradient gradientMax = new Gradient();
+        gradientMax.SetKeys(gradientColorKeyMax, gradientAlphaKeyMax);
+
+
+        //Create Color from Gradient
+        ParticleSystem.MinMaxGradient color = new ParticleSystem.MinMaxGradient();
+        color.mode = ParticleSystemGradientMode.TwoGradients;
+        color.gradientMin = gradientMin;
+        color.gradientMax = gradientMax;
+
+        //Assign the color to the particle
+        ParticleSystem.ColorOverLifetimeModule main = GetComponent<ParticleSystem>().colorOverLifetime;
+        main.color = new ParticleSystem.MinMaxGradient(gradientMin , gradientMax);
+        //main.startColor = color;
+    }
+
+    // Vertical色変更
+    private void ChangeColorVertical(bool bGreen)
+    {
+        Color col;
+        if (bGreen == true)
+            col = Color.green;
+        else
+            col = Color.yellow;
+        // ――――――――――――――――――――――――――――――――――――
+        //
+        // ――――――――――――――――――――――――――――――――――――
+        //Create Gradient key Min
+        GradientColorKey[] gradientColorKeyMin;
+        gradientColorKeyMin = new GradientColorKey[2];
+        gradientColorKeyMin[0].color = col;
+        gradientColorKeyMin[0].time = 0f;
+        gradientColorKeyMin[1].color = col;
+        gradientColorKeyMin[1].time = 1f;
+
+        //Create Gradient alpha Min
+        GradientAlphaKey[] gradientAlphaKeyMin;
+        gradientAlphaKeyMin = new GradientAlphaKey[4];
+        gradientAlphaKeyMin[0].alpha = 0.0f;
+        gradientAlphaKeyMin[0].time = 0.0f;
+        gradientAlphaKeyMin[1].alpha = (float)255.0f / (float)255.0f;
+        gradientAlphaKeyMin[1].time = 0.305f;
+        gradientAlphaKeyMin[2].alpha = 173.0f / 255.0f;
+        gradientAlphaKeyMin[2].time = 0.702f;
+        gradientAlphaKeyMin[3].alpha = 0f;
+        gradientAlphaKeyMin[3].time = 1f;
+
+        //Create Gradient key Max
+        GradientColorKey[] gradientColorKeyMax;
+        gradientColorKeyMax = new GradientColorKey[2];
+        gradientColorKeyMax[0].color = col;
+        gradientColorKeyMax[0].time = 0f;
+        gradientColorKeyMax[1].color = col;
+        gradientColorKeyMax[1].time = 1f;
+
+        //Create Gradient alpha Max
+        GradientAlphaKey[] gradientAlphaKeyMax;
+        gradientAlphaKeyMax = new GradientAlphaKey[4];
+        gradientAlphaKeyMax[0].alpha = 0.0f;
+        gradientAlphaKeyMax[0].time = 0.0f;
+        gradientAlphaKeyMax[1].alpha = (float)255.0f / (float)255.0f;
+        gradientAlphaKeyMax[1].time = 0.305f;
+        gradientAlphaKeyMax[2].alpha = 173.0f / 255.0f;
+        gradientAlphaKeyMax[2].time = 0.702f;
+        gradientAlphaKeyMax[3].alpha = 0f;
+        gradientAlphaKeyMax[3].time = 1f;
+
+        //Create Gradient Min
+        Gradient gradientMin = new Gradient();
+        gradientMin.SetKeys(gradientColorKeyMin, gradientAlphaKeyMin);
+
+        //Create Gradient Max
+        Gradient gradientMax = new Gradient();
+        gradientMax.SetKeys(gradientColorKeyMax, gradientAlphaKeyMax);
+
+
+        //Create Color from Gradient
+        ParticleSystem.MinMaxGradient color = new ParticleSystem.MinMaxGradient();
+        color.mode = ParticleSystemGradientMode.TwoGradients;
+        color.gradientMin = gradientMin;
+        color.gradientMax = gradientMax;
+
+        //Assign the color to the particle
+        ParticleSystem.ColorOverLifetimeModule main = Vertical.GetComponent<ParticleSystem>().colorOverLifetime;
+        main.color = new ParticleSystem.MinMaxGradient(gradientMin, gradientMax);
+        //main.startColor = color;
+    }
+
+    // GlowBits色変更
+    private void ChangeColorGlowBits(bool bGreen)
+    {
+        Color col;
+        if (bGreen == true)
+            col = Color.green;
+        else
+            col = Color.yellow;
+        // ――――――――――――――――――――――――――――――――――――
+        //
+        // ――――――――――――――――――――――――――――――――――――
+        //Create Gradient key Min
+        GradientColorKey[] gradientColorKeyMin;
+        gradientColorKeyMin = new GradientColorKey[2];
+        gradientColorKeyMin[0].color = col;
+        gradientColorKeyMin[0].time = 0f;
+        gradientColorKeyMin[1].color = col;
+        gradientColorKeyMin[1].time = 1f;
+
+        //Create Gradient alpha Min
+        GradientAlphaKey[] gradientAlphaKeyMin;
+        gradientAlphaKeyMin = new GradientAlphaKey[4];
+        gradientAlphaKeyMin[0].alpha = 0.0f;
+        gradientAlphaKeyMin[0].time = 0.0f;
+        gradientAlphaKeyMin[1].alpha = (float)255.0f / (float)255.0f;
+        gradientAlphaKeyMin[1].time = 0.305f;
+        gradientAlphaKeyMin[2].alpha = 173.0f / 255.0f;
+        gradientAlphaKeyMin[2].time = 0.702f;
+        gradientAlphaKeyMin[3].alpha = 0f;
+        gradientAlphaKeyMin[3].time = 1f;
+
+        //Create Gradient key Max
+        GradientColorKey[] gradientColorKeyMax;
+        gradientColorKeyMax = new GradientColorKey[2];
+        gradientColorKeyMax[0].color = col;
+        gradientColorKeyMax[0].time = 0f;
+        gradientColorKeyMax[1].color = col;
+        gradientColorKeyMax[1].time = 1f;
+
+        //Create Gradient alpha Max
+        GradientAlphaKey[] gradientAlphaKeyMax;
+        gradientAlphaKeyMax = new GradientAlphaKey[4];
+        gradientAlphaKeyMax[0].alpha = 0.0f;
+        gradientAlphaKeyMax[0].time = 0.0f;
+        gradientAlphaKeyMax[1].alpha = (float)255.0f / (float)255.0f;
+        gradientAlphaKeyMax[1].time = 0.305f;
+        gradientAlphaKeyMax[2].alpha = 173.0f / 255.0f;
+        gradientAlphaKeyMax[2].time = 0.702f;
+        gradientAlphaKeyMax[3].alpha = 0f;
+        gradientAlphaKeyMax[3].time = 1f;
+
+        //Create Gradient Min
+        Gradient gradientMin = new Gradient();
+        gradientMin.SetKeys(gradientColorKeyMin, gradientAlphaKeyMin);
+
+        //Create Gradient Max
+        Gradient gradientMax = new Gradient();
+        gradientMax.SetKeys(gradientColorKeyMax, gradientAlphaKeyMax);
+
+
+        //Create Color from Gradient
+        ParticleSystem.MinMaxGradient color = new ParticleSystem.MinMaxGradient();
+        color.mode = ParticleSystemGradientMode.TwoGradients;
+        color.gradientMin = gradientMin;
+        color.gradientMax = gradientMax;
+
+        //Assign the color to the particle
+        ParticleSystem.ColorOverLifetimeModule main = GlowBits.GetComponent<ParticleSystem>().colorOverLifetime;
+        main.color = new ParticleSystem.MinMaxGradient(gradientMin, gradientMax);
+        // main.startColor = color;
+    }
+
+    // BigGlow色変更
+    private void ChangeColorBigGlow(bool bGreen)
+    {
+        Color col;
+        if (bGreen == true)
+            col = Color.green;
+        else
+            col = Color.yellow;
+        // ――――――――――――――――――――――――――――――――――――
+        //
+        // ――――――――――――――――――――――――――――――――――――
+        //Create Gradient key Min
+        GradientColorKey[] gradientColorKeyMin;
+        gradientColorKeyMin = new GradientColorKey[2];
+        gradientColorKeyMin[0].color = col;
+        gradientColorKeyMin[0].time = 0f;
+        gradientColorKeyMin[1].color = col;
+        gradientColorKeyMin[1].time = 1f;
+
+        //Create Gradient alpha Min
+        GradientAlphaKey[] gradientAlphaKeyMin;
+        gradientAlphaKeyMin = new GradientAlphaKey[4];
+        gradientAlphaKeyMin[0].alpha = 0.0f;
+        gradientAlphaKeyMin[0].time = 0.0f;
+        gradientAlphaKeyMin[1].alpha = (float)255.0f / (float)255.0f;
+        gradientAlphaKeyMin[1].time = 0.305f;
+        gradientAlphaKeyMin[2].alpha = 173.0f / 255.0f;
+        gradientAlphaKeyMin[2].time = 0.702f;
+        gradientAlphaKeyMin[3].alpha = 0f;
+        gradientAlphaKeyMin[3].time = 1f;
+
+        //Create Gradient key Max
+        GradientColorKey[] gradientColorKeyMax;
+        gradientColorKeyMax = new GradientColorKey[2];
+        gradientColorKeyMax[0].color = col;
+        gradientColorKeyMax[0].time = 0f;
+        gradientColorKeyMax[1].color = col;
+        gradientColorKeyMax[1].time = 1f;
+
+        //Create Gradient alpha Max
+        GradientAlphaKey[] gradientAlphaKeyMax;
+        gradientAlphaKeyMax = new GradientAlphaKey[4];
+        gradientAlphaKeyMax[0].alpha = 0.0f;
+        gradientAlphaKeyMax[0].time = 0.0f;
+        gradientAlphaKeyMax[1].alpha = (float)255.0f / (float)255.0f;
+        gradientAlphaKeyMax[1].time = 0.305f;
+        gradientAlphaKeyMax[2].alpha = 173.0f / 255.0f;
+        gradientAlphaKeyMax[2].time = 0.702f;
+        gradientAlphaKeyMax[3].alpha = 0f;
+        gradientAlphaKeyMax[3].time = 1f;
+
+        //Create Gradient Min
+        Gradient gradientMin = new Gradient();
+        gradientMin.SetKeys(gradientColorKeyMin, gradientAlphaKeyMin);
+
+        //Create Gradient Max
+        Gradient gradientMax = new Gradient();
+        gradientMax.SetKeys(gradientColorKeyMax, gradientAlphaKeyMax);
+
+
+        //Create Color from Gradient
+        ParticleSystem.MinMaxGradient color = new ParticleSystem.MinMaxGradient();
+        color.mode = ParticleSystemGradientMode.TwoGradients;
+        color.gradientMin = gradientMin;
+        color.gradientMax = gradientMax;
+
+        //Assign the color to the particle
+        ParticleSystem.ColorOverLifetimeModule main = BigGlow.GetComponent<ParticleSystem>().colorOverLifetime;
+        main.color = new ParticleSystem.MinMaxGradient(gradientMin, gradientMax);
+        //main.startColor = color;
+    }
+}
