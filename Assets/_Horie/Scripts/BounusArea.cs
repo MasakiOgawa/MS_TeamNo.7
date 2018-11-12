1fca7c38dddc39bba64fbd983cf98f7f4dca8513
HORIE_YUKI_DESKPC <vipstar.final@gmail.com>
2018-11-12 21:03:14 +0900

    MAP更新

---
+++ Assets/_Horie/Scripts/BounusArea.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounusArea : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Area1;
+    [SerializeField] private GameObject Area2;
+    [SerializeField] private GameObject Area3;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Init
+    static void Init()
+    {
+        
+
+    }
+
+}
