de49b764d120943d4a204de046ce7039e3f87c08
MYCOMPUTER\MasakiOgawa <jrp6exu3f@outlook.jp>
2018-11-26 05:16:04 +0900

    敵キャラ16種類のテクスチャをランダムに変更

---
+++ Assets/RandomMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaterial : MonoBehaviour
+{
+	// ランダムで表示したいマテリアル情報
+	[SerializeField] Material[] materials;
+
+	private void Awake()
+	{
+		// 乱数設定
+		int rand;
+		rand = Random.Range(0, materials.Length);
+
+		// マテリアルをランダムに設定
+		GetComponent<SkinnedMeshRenderer>().material = new Material(materials[rand]);
+	}
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
